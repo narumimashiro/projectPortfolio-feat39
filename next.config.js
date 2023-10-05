dd43a78883aadd81b80c6f525df1b80f64e33884
Narumi_Mashiro <narunarumashiro96@gmail.com>
2023-10-05 23:52:47 +0900

    next configでAPIフォルダのルーティングを設定

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
+  },
   images: {
     domains: ['naru396-aws-storage.s3-ap-northeast-1.amazonaws.com']
   },
