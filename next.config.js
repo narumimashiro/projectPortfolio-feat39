ea8ef28d802636b8d3d2f3d6fa12ae9adb3cb525
Narumi_Mashiro <narunarumashiro96@gmail.com>
2023-10-06 23:18:30 +0900

    option clear

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['naru396-aws-storage.s3-ap-northeast-1.amazonaws.com']
+    domains: ['naru396-aws-storage.s3-ap-northeast-1.amazonaws.com'],
+    unoptimized: true,
   },
 }
 
